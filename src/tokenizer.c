263f1e1cb7ba94878ca0e76a0cf0130780e6e9ce
hernangarcia1517 <thisistacopecks@gmail.com>
2021-09-28 11:52:58 -0600

    testing connection to github after not having it for awhile

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -22,14 +22,27 @@ int non_space_char(char c)
 
 char *word_start(char *str)
 {
-  int lengthOfString = str.length;
-  printf("lengh of string: %d\n", lengthOfString);
-  return str;
+  int size = 11;
+  int countChars = 0;
+  for(int i = 0; i < size; i++)
+  {
+    if(space_char(str[i]) == 1)
+    {
+      char newStr[size - i];
+      countChars = 1;
+    }
+    if(countChars == 1)
+    {
+      newStr[size - (size - i)] = str[i];
+    }
+  }
+  printf("new string: %s\n", newStr);
+  return newStr;
 }
 
 int main(char args)
 {
-  char testing[5] = {'t', 'e', 's', 't', '\0'};
+  char testing[11] = {'t', 'e', 's', 't', ' ', 't', 'e', 's', 't', '2', '\0'};
   int test1 = non_space_char('c');
   int test2 = space_char(' ');
   int test3 = space_char('p');
