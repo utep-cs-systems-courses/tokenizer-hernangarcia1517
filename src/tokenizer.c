91cc54964465a852b1ab6cb38c3f73554cdc1635
hernangarcia1517 <thisistacopecks@gmail.com>
2021-09-29 22:22:40 -0600

    added part c

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int string_length(char *word)
+{
+  char *numberOfCharacters = word;
+  while(*word)
+  {
+    word++;
+  }
+  return word - numberOfCharacters;
+}
+
 int space_char(char c)
 {
   if(c == ' ' || c == '\t' || c == '\n')
@@ -22,22 +33,111 @@ int non_space_char(char c)
 
 char *word_start(char *str)
 {
-  int size = 11;
-  int countChars = 0;
-  for(int i = 0; i < size; i++)
+  int out = 0;
+  int state = out;
+  int start = 0;
+  int length = string_length(str);
+
+  for(int i = 0;i <= length; i++)
+  {
+    if(str[i] == '\n')
+    {
+      start = 1;
+      i = length;
+    }
+    else if(str[i] == ' ' || str[i] == '\t')
+    {
+      state = out;
+    }
+    else if(state == out)
+    {
+      start = i;
+      i = length;
+    }
+  }
+  return &str[start];
+}
+
+char *word_end(char *str)
+{
+  int in = 1;
+  int out = 0;
+  int state = out;
+  int end = 0;
+  int word_count = 0;
+  int length = string_length(str);
+
+  for(int i = 0; i <= length; i++)
   {
     if(space_char(str[i]) == 1)
     {
-      char newStr[size - i];
-      countChars = 1;
+      if(word_count == 1)
+      {
+        state = out;
+        end = i - 1;
+        i = length;
+      }
+      else
+      {
+	state = out;
+      }
     }
-    if(countChars == 1)
+    else if(state == out)
     {
-      newStr[size - (size - i)] = str[i];
+      state = in;
+      word_count++;
     }
   }
-  printf("new string: %s\n", newStr);
-  return newStr;
+  return &str[end];
+}
+
+int count_words(char *str)
+{
+  int in = 1;
+  int out = 0;
+  int state = out;
+  int word_count = 0;
+  int length = string_length(str);
+
+  for(int i = 0; i <= length; i++)
+  {
+    if(space_char(str[i]) == 1)
+    {
+      state = out;
+    }
+    else if(state == out)
+    {
+      state = in;
+      word_count++;
+    }
+  }
+  return word_count;
+}
+
+void free_tokens(char **word)
+{
+  int i = 0;
+  while(word[i] != NULL)
+  {
+    free(word[i]);
+    i++;
+  }
+  free(word);
+}
+
+void print_tokens(char **word)
+{
+  int i = 0;
+  while(word[i] != NULL)
+  {
+    printf("%s\n", word[i]);
+    i++;
+  }
+}
+
+char **tokenize(char* str)
+{
+  
 }
 
 int main(char args)
