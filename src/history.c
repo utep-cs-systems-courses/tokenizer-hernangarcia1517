44ceba2f46f60b32d0c5859c2ae8cb8c717a5d98
hernangarcia1517 <thisistacopecks@gmail.com>
2021-10-05 19:00:47 -0600

    initialized history and created add history

--- src/history.c
+++ src/history.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "history.h"
 
 List* init_history()
 {
+  List history;
+  history = (List) malloc(sizeof(List));
+  history->root = NULL;
+  return history;
 }
 
 void add_history(List *list, char *str)
 {
+  Node new = (Node) malloc(sizeof(Node));
+
+  if(list->root == NULL)
+  {
+    new->id = 1;
+    list->root = new;
+  }
+  else
+  {
+    Node currentNode = list->root;
+    while(currentNode->next != NULL)
+    {
+      currentNode = currentNode->next;
+    }
+    new->id = currentNode->id+1;
+    currentNode->next = new;
+  }
+  // add string to linked list
+  new->str = str;
+  new->next = NULL;
 }
 
 char *get_history(List *list, int id)
 {
+  
 }
 
 void print_history(List *list)
